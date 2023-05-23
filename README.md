c3b0c925e0080c9819031b0d9120bb8a3d0f2595
Hoakin24 <89337901+Hoakin24@users.noreply.github.com>
2023-05-23 23:42:37 +0800

    predictor & dataset creator

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# gesture-prediction
-Prediction of gestures using Mediapipe and sklearn, providing customized outputs based on recognized gestures.
+# Gesture Recognition with Mediapipe and sklearn
+
+This repository contains code that utilizes Mediapipe and sklearn to predict gestures based on hand landmarks captured from a camera feed. The predicted gestures trigger corresponding outputs, allowing for gesture-based interaction with applications.
+
+## Requirements
+- opencv-python
+- mediapipe
+- keyboard
+- pyautogui
+- statistics
+- scikit-learn
+- pandas
+
+Make sure to install these dependencies before running the code.
+
+## Usage
+
+1. Open the camera feed by running the code.
+2. The code captures the hand landmarks using Mediapipe's pose estimation and displays them on the screen.
+3. The hand landmarks are used to predict the gesture using a Random Forest classifier trained with sklearn.
+4. Depending on the recognized gesture, the code sends keyboard commands using `pyautogui`.
+   - Gesture "up" triggers the 'w' key.
+   - Gesture "down" triggers the 's' key.
+   - Gesture "left" triggers the 'a' key.
+   - Gesture "right" triggers the 'd' key.
+   - Gesture "stop" releases all pressed keys ('w', 'a', 's', 'd').
+
+## Dataset Creation
+The `dataset_creator.py` script captures video frames from the camera feed, detects hand landmarks using Mediapipe, and allows you to record hand landmarks for different gestures by pressing the corresponding keys. The recorded hand landmarks and corresponding gestures are saved in a CSV file named "dataset.csv."
+
+Usage Tips:
+
+- Run the `dataset_creator.py` script and follow the on-screen instructions to record hand landmarks for each gesture of interest.
+- Make sure your hand is properly positioned within the camera's field of view for accurate hand landmark detection.
+- Press the corresponding keys to record hand landmarks for different gestures.
+- You can modify the script to add more gestures or customize the recording process according to your needs.
+
+## Training
+The Random Forest classifier is trained using the provided dataset. It splits the dataset into training and testing sets, and the model is trained on the training set. The accuracy of the model is computed using the testing set (not necessary for runtime prediction).
+
+## Additional Notes
+- The code continuously captures frames from the camera feed and performs gesture prediction.
+- Make sure to position your hand within the camera's field of view for accurate hand landmark detection.
+
+This will be passing this as the Final Project for CS50x. 
+
+I wanted to try if I can make in-game characters move with my hand and a camera, and I did!
+
+Feel free to modify the code according to your requirements and dataset. Enjoy exploring gesture recognition with Mediapipe and sklearn!
---
+++ predictor.py
@@ -0,0 +1,126 @@
+# Import libraries
+import pyautogui
+from statistics import mode
+import cv2
+import mediapipe as mp
+from mediapipe import solutions
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Open the camera
+cap = cv2.VideoCapture(0)
+
+# Initialize Mediapipe hands and drawing utilities
+mpHands = solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+# Load the dataset
+d = pd.read_csv("dataset.csv", sep=',')
+
+# Prepare the data for training
+X = d.drop('gesture', axis=1)
+Y = d['gesture']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=3)
+
+# Train the Random Forest model
+model = RandomForestClassifier()
+model.fit(X_train, Y_train)
+
+# These two lines are only used to measure the accuracy (not needed)
+# X_test_prediction = model.predict(X_test)
+# test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+# Perform predictions
+def predict(hand_landmarks):
+    center_x = (hand_landmarks.landmark[0].x + hand_landmarks.landmark[5].x + hand_landmarks.landmark[17].x)/3 # Kunin yung position ng palm
+    center_y = (hand_landmarks.landmark[0].y + hand_landmarks.landmark[5].y + hand_landmarks.landmark[17].y)/3 # Kagaya ng taas pero Y axis naman
+
+    thumb_tip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_TIP].x
+    thumb_tip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_TIP].y
+    thumb_ip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_IP].x
+    thumb_ip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_IP].y
+    thumb_mcp_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_MCP].x
+    thumb_mcp_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_MCP].y
+    thumb_cmc_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_CMC].x
+    thumb_cmc_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.THUMB_CMC].y
+
+    index_tip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x
+    index_tip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y
+    index_dip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_DIP].x
+    index_dip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_DIP].y
+    index_pip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_PIP].x
+    index_pip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_PIP].y
+    index_mcp_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_MCP].x
+    index_mcp_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_MCP].y
+    
+    middle_tip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].x
+    middle_tip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].y
+    middle_dip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_DIP].x
+    middle_dip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_DIP].y
+    middle_pip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_PIP].x
+    middle_pip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_PIP].y
+    middle_mcp_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].x
+    middle_mcp_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].y
+    
+    ring_tip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_TIP].x
+    ring_tip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_TIP].y
+    ring_dip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_DIP].x
+    ring_dip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_DIP].y
+    ring_pip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_PIP].x
+    ring_pip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_PIP].y
+    ring_mcp_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_MCP].x
+    ring_mcp_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.RING_FINGER_MCP].y
+    
+    pinky_tip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_TIP].x
+    pinky_tip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_TIP].y
+    pinky_dip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_DIP].x
+    pinky_dip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_DIP].y
+    pinky_pip_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_PIP].x
+    pinky_pip_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_PIP].y
+    pinky_mcp_x = center_x - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_MCP].x
+    pinky_mcp_y = center_y - hand_landmarks.landmark[mpHands.HandLandmark.PINKY_MCP].y
+
+    df = pd.DataFrame([[thumb_tip_x, thumb_tip_y, thumb_ip_x, thumb_ip_y, thumb_mcp_x, thumb_mcp_y, thumb_cmc_x, thumb_cmc_y, index_tip_x, index_tip_y, index_dip_x, index_dip_y, index_pip_x, index_pip_y, index_mcp_x, index_mcp_y, middle_tip_x, middle_tip_y, middle_dip_x, middle_dip_y, middle_pip_x, middle_pip_y, middle_mcp_x, middle_mcp_y, ring_tip_x, ring_tip_y, ring_dip_x, ring_dip_y, ring_pip_x, ring_pip_y, ring_mcp_x, ring_mcp_y, pinky_tip_x, pinky_tip_y, pinky_dip_x, pinky_dip_y, pinky_pip_x, pinky_pip_y, pinky_mcp_x, pinky_mcp_y]], columns=['thumb_tip_x', 'thumb_tip_y', 'thumb_ip_x', 'thumb_ip_y', 'thumb_mcp_x', 'thumb_mcp_y', 'thumb_cmc_x', 'thumb_cmc_y', 'index_tip_x', 'index_tip_y', 'index_dip_x', 'index_dip_y', 'index_pip_x', 'index_pip_y', 'index_mcp_x', 'index_mcp_y', 'middle_tip_x', 'middle_tip_y', 'middle_dip_x', 'middle_dip_y', 'middle_pip_x', 'middle_pip_y', 'middle_mcp_x', 'middle_mcp_y', 'ring_tip_x', 'ring_tip_y', 'ring_dip_x', 'ring_dip_y', 'ring_pip_x', 'ring_pip_y', 'ring_mcp_x', 'ring_mcp_y', 'pinky_tip_x', 'pinky_tip_y', 'pinky_dip_x', 'pinky_dip_y', 'pinky_pip_x', 'pinky_pip_y', 'pinky_mcp_x', 'pinky_mcp_y'])
+    return model.predict(df)
+
+# Main loop
+while True:
+    # Read frame from the camera
+    success, image = cap.read()
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks: # working with each hand
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                if id == 20 :
+                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Output", image)
+    cv2.waitKey(1)
+
+    if not results.multi_hand_world_landmarks:
+        continue
+
+    for hand_landmarks in results.multi_hand_landmarks:
+        gesture = predict(hand_landmarks)[0]
+        if gesture == "up":
+            pyautogui.keyDown('w')
+        elif gesture == "down":
+            pyautogui.keyDown('s')
+        elif gesture == "left":
+            pyautogui.keyDown('a')
+        elif gesture == "right":
+            pyautogui.keyDown('d')
+        elif gesture == "stop":
+            pyautogui.keyUp('w')
+            pyautogui.keyUp('a')
+            pyautogui.keyUp('s')
+            pyautogui.keyUp('d')
